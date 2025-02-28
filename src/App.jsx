bd61f4e2cd1e343cc12652d2b3a3b0f71a4ebc1d
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2025-02-28 15:01:03 -0500

    Add UI with header and samosa image

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: 0</h2>
+        <img className="samosa" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Samosa_with_chutney.jpg/600px-Samosa_with_chutney.jpg" alt="Samosa" />
+      </div>
     </div>
   );
 }
