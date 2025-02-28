c8bc1babbaff2fce4d063edff8e0b935a1c12364
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2025-02-28 15:01:40 -0500

    Implement counter functionality and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
+
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count: 0</h2>
-        <img className="samosa" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Samosa_with_chutney.jpg/600px-Samosa_with_chutney.jpg" alt="Samosa" />
+        <h2>Count: {count}</h2>
+        <img className="samosa" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Samosa_with_chutney.jpg/600px-Samosa_with_chutney.jpg" alt="Samosa" onClick={updateCount} />
       </div>
     </div>
   );
